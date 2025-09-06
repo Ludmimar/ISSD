9a6c959d76684346563876a66ef1022180dac522
Ludmila Martos <95381011+Ludmimar@users.noreply.github.com>
2025-09-06 11:08:55 -0300

    clase08

---
+++ Programacion 1/CLASE 08/Desempeño 62.py
@@ -0,0 +1,14 @@
+def calcular_cuadrado():
+    print("Calculo del Cuadrado")
+    valor=int(input("Ingrese un entero:"))
+    cuadrado=valor*valor
+    print("El cuadrado es",cuadrado)
+def calcular_producto():
+    print("Calculo del producto de dos numeros")
+    valor1=int(input("Ingrese primer valor:"))
+    valor2=int(input("Ingrese segundo valor:"))
+    producto=valor1*valor2
+    print("El producto de los valores es:",producto)
+# bloque principal
+calcular_cuadrado()
+calcular_producto()
